498eb855f4246e02efc54a1c1bbb22516e75658a
Alston Chan <51868695+AlstonChan@users.noreply.github.com>
2023-08-04 11:19:39 +0800

    Rollback to exclude tinacms cloud

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '5b2d218ff47fa5b69756095608b5afe6d0d4c0bb', queries });
-export default client;
-  
--- tina/config.ts
+++ tina/config.ts
@@ -25,8 +25,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: process.env.TINACMS_CLIENT_ID, // Get this from tina.io
-  token: process.env.TINACMS_TOKEN, // Get this from tina.io
+  clientId: null, // Get this from tina.io
+  token: null, // Get this from tina.io
   build: {
     outputFolder: "admin",
     publicFolder: "public",
